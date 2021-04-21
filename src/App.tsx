127ad78c9f84629182d5d0485a5cc79457f0c1de
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-04-21 09:43:53 +0200

    Update with new charts:D

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,7 @@ function App() {
         <h2>Weather for the next days:</h2>
         <MetApi.Threshold width={800} height={300} />
         <h2>Population of somewhere</h2>
-        <SsbBefolkning.Line
-          width={800}
-          height={300}
-          strokeColor={"red"}
-          colorBottom={"aqua"}
-        />
+        <SsbBefolkning.Line width={800} height={300} strokeColor={"red"} />
         <h2>
           Read more about how to use this project{" "}
           <a href="https://github.com/IT2901-SINTEF01/frontend/wiki/%22data-visualisation-platform%22">
