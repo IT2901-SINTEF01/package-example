655d563875408b628b32e673969a546ef40003a4
Jonathan Brooks <brooks.jonathan1@gmail.com>
2021-04-23 15:06:31 +0200

    Update with new info:)

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,16 @@ function App() {
     <VisualiserProvider>
       <div className="App">
         <h1>Hi guys, check out our epic package library!</h1>
-        <h2>Weather for the next days:</h2>
-        <MetApi.Threshold width={800} height={300} />
-        <h2>Population of somewhere</h2>
-        <SsbBefolkning.Line width={800} height={300} strokeColor={"red"} />
+        <h2>Weather for Trondheim in the coming days:</h2>
+        <MetApi.Line width={600} height={200} lat={63} lon={10} />
+        <h2>Population growth of Bærum kommune</h2>
+        <SsbBefolkning.Threshold
+          width={600}
+          height={200}
+          yLabel={"Population"}
+          aboveThresholdColor={"#00BFFF"}
+          municipalities={["K-3024"]}
+        />
         <h2>
           Read more about how to use this project{" "}
           <a href="https://github.com/IT2901-SINTEF01/frontend/wiki/%22data-visualisation-platform%22">
